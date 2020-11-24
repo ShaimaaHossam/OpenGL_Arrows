20390cf7cdfa47e4234662890cd74925b01034fd
ShaimaaHossam <40139825+ShaimaaHossam@users.noreply.github.com>
2020-11-24 13:21:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,33 +24,30 @@ void specKeyboard(int key, int x, int y)
 {
 if(key==GLUT_KEY_UP)
 {
-
 up=1; down=0; left=0; right=0;
 }
 if(key== GLUT_KEY_DOWN)
 {
-
 up=0; down=1; left=0; right=0;
 }
 if(key== GLUT_KEY_LEFT)
 {
-
 up=0; down=0; left=1; right=0;
 }
 if(key== GLUT_KEY_RIGHT)
 {
-
 up=0; down=0; left=0; right=1;
 }
 
-
 glutPostRedisplay();
+      
 }
 
 
 
 void display(void){
-      glClear(GL_COLOR_BUFFER_BIT);
+      
+glClear(GL_COLOR_BUFFER_BIT);
 
 glColor3f(0.0, 0.0, 0.0);
 glBegin(GL_POLYGON);
@@ -61,7 +58,6 @@ glVertex2f(centerX-sqWid/2, centerY+sqWid/2);
 glEnd();
 
 if(right ==1){
-        \
 glBegin(GL_POLYGON);
 glVertex2f(60,70);
 glVertex2f(60,30);
@@ -69,7 +65,6 @@ glVertex2f(70,50);
 glEnd();
 }
 if(left ==1){
-
 glBegin(GL_POLYGON);
 glVertex2f(40,70);
 glVertex2f(40,30);
@@ -77,7 +72,6 @@ glVertex2f(30,50);
 glEnd();
 }
 if(up ==1){
-
 glBegin(GL_POLYGON);
 glVertex2f(70,60);
 glVertex2f(30,60);
@@ -85,7 +79,6 @@ glVertex2f(50,70);
 glEnd();
 }
 if(down ==1){
-
 glBegin(GL_POLYGON);
 glVertex2f(70,40);
 glVertex2f(30,40);
@@ -97,16 +90,13 @@ glEnd();
 
 glutSwapBuffers();
       glFlush();
-
 }
 void init(){
       glClearColor(1.0,1.0,1.0,1.0);
       glMatrixMode(GL_PROJECTION);
       gluOrtho2D(0.0,logWidth,0.0,logHeight);
-
 }
 int main(int argc, char* argv[]){
-
       glutInit( &argc, argv);
       glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB);
       glutInitWindowPosition( 100, 100);
